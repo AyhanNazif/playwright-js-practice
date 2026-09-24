5d4b480eaa031165c85f633e03f4e3beb477d431
ayhannazif <ayhannazif91@gmail.com>
2026-09-24 21:37:49 +0300

    changed path

--- SauceDemo.test.js
+++ SauceDemo.test.js
@@ -1,10 +1,10 @@
 const{ test, chromium, expect } = require('@playwright/test');
-const{HomePage} = require('../JS_Practice/pages/HomePage');
-const{InventoryPage} = require('../JS_Practice/pages/InventoryPage');
-const{CartPage} = require('../JS_Practice/pages/CartPage');
-const{CheckoutStepOne} = require('../JS_Practice/pages/CheckoutStepOne');
-const{CheckoutStepTwo} = require('../JS_Practice/pages/CheckoutStepTwo');
-const { CheckoutCompletePage } = require('../JS_Practice/pages/CheckoutCompletePage');
+const{HomePage} = require('./pages/HomePage');
+const{InventoryPage} = require('./pages/InventoryPage');
+const{CartPage} = require('./pages/CartPage');
+const{CheckoutStepOne} = require('./pages/CheckoutStepOne');
+const{CheckoutStepTwo} = require('./pages/CheckoutStepTwo');
+const { CheckoutCompletePage } = require('./pages/CheckoutCompletePage');
 
 let browser;
 let context;
